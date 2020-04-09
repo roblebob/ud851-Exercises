8f0067f3899937a9e1a9480c79ddb94cb5eac657
roblebob <robert.koppisch@gmail.com>
2020-04-09 22:59:01 +0200

    followed steps ...

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/MainActivity.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/MainActivity.java
@@ -15,15 +15,21 @@
  */
 package com.example.android.background;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.example.android.background.sync.ReminderTasks;
+import com.example.android.background.sync.WaterReminderIntentService;
 import com.example.android.background.utilities.PreferenceUtilities;
+
 
 public class MainActivity extends AppCompatActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
@@ -68,8 +74,7 @@ public class MainActivity extends AppCompatActivity implements
         int chargingReminders = PreferenceUtilities.getChargingReminderCount(this);
         String formattedChargingReminders = getResources().getQuantityString(
                 R.plurals.charge_notification_count, chargingReminders, chargingReminders);
-        mChargingCountDisplay.setText(formattedChargingReminders);
-
+        mChargingCountDisplay .setText(formattedChargingReminders);
     }
 
     /**
@@ -79,9 +84,13 @@ public class MainActivity extends AppCompatActivity implements
         if (mToast != null) mToast.cancel();
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
         mToast.show();
-        // TODO (15) Create an explicit intent for WaterReminderIntentService
-        // TODO (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
-        // TODO (17) Call startService and pass the explicit intent you just created
+
+        // _TODO (15) Create an explicit intent for WaterReminderIntentService (... put IntentService inside  => an INTENCEPTION)
+        Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
+        // _TODO (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
+        incrementWaterCountIntent .setAction( ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+        // _TODO (17) Call startService and pass the explicit intent you just created
+        startService( incrementWaterCountIntent);
     }
 
     @Override
@@ -98,10 +107,7 @@ public class MainActivity extends AppCompatActivity implements
      */
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (PreferenceUtilities.KEY_WATER_COUNT.equals(key)) {
-            updateWaterCount();
-        } else if (PreferenceUtilities.KEY_CHARGING_REMINDER_COUNT.equals(key)) {
-            updateChargingReminderCount();
-        }
+        if      (PreferenceUtilities.KEY_WATER_COUNT.equals(key))             { updateWaterCount(); }
+        else if (PreferenceUtilities.KEY_CHARGING_REMINDER_COUNT.equals(key)) { updateChargingReminderCount(); }
     }
 }
--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -13,11 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.background.sync;
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import android.app.IntentService;
+import android.content.Intent;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+import androidx.annotation.Nullable;
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTask and pass in the action to be performed
+// _TODO (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
+
+    //  _TODO (10) Create a default constructor that calls super with the name of this class
+    public WaterReminderIntentService() { super("WaterReminderIntentService"); }
+
+
+    //  _TODO (11) Override onHandleIntent
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+//      _TODO (12) Get the action from the Intent that started this Service
+        String action = intent.getAction();
+//      _TODO (13) Call ReminderTasks.executeTask and pass in the action to be performed
+        ReminderTasks .executeTask(this, action);
+    }
+}
+
+
+
